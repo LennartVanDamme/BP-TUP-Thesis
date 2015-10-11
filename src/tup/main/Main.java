f870448bd14923ad1f2a248484028c4a086b6b01
Lennart Van Damme <lennart.vandamme@gmail.com>
2015-10-11 12:11:47 +0200

    Signed-off-by: Lennart Van Damme <lennart.vandamme@gmail.com>

--- src/tup/main/Main.java
+++ src/tup/main/Main.java
@@ -1,82 +1,33 @@
 package tup.main;
 
-import java.io.File;
-
-import com.sun.xml.internal.ws.runtime.config.TubeFactoryConfig;
-
-import java.io.File;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		File dir = new File("/Users/Lennart/git/BP-TUP-Thesis/instances/");
-		File [] instanceFiles = dir.listFiles();
-		
-		/*for(File instanceFile : instanceFiles){
-			TUPSolver solver = new TUPSolver(4);
-	    	solver.readInstance(instanceFile.getAbsolutePath());
-	    	solver.gamesPerRound = solver.problem.nTeams / 2;
-	    	solver.problem.q1 = solver.problem.nUmpires;
-            solver.problem.q2 = solver.problem.nUmpires / 2;
-	    	solver.solve(900);
-	    	solver.solution();
-	    	solver.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/annealing4/15min/"+extracFile(instanceFile.getName())+"q1="+
-	    			solver.problem.q1+"&q2="+solver.problem.q2+"(15min)");
-		}*/
-		
-		/*for(File instanceFile : instanceFiles){
-			TUPSolver solver = new TUPSolver(7);
-	    	solver.readInstance(instanceFile.getAbsolutePath());
-	    	solver.gamesPerRound = solver.problem.nTeams / 2;
-	    	solver.problem.q1 = solver.problem.nUmpires;
-            solver.problem.q2 = solver.problem.nUmpires / 2;
-	    	solver.solve(900);
-	    	solver.solution();
-	    	solver.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/annealing7/15min/"+extracFile(instanceFile.getName())+"q1="+
-	    			solver.problem.q1+"&q2="+solver.problem.q2+"(15min)");
-		}*/
-	
-		for(File instanceFile : instanceFiles){
-			TUPSolver solver = new TUPSolver(7);
-	    	solver.readInstance(instanceFile.getAbsolutePath());
-	    	solver.gamesPerRound = solver.problem.nTeams / 2;
-	    	solver.problem.q1 = solver.problem.nUmpires;
-            solver.problem.q2 = solver.problem.nUmpires / 2;
-	    	solver.solve(300);
-	    	solver.solution();
-	    	solver.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/annealing7/5min/"+extracFile(instanceFile.getName())+"q1="+
-	    			solver.problem.q1+"&q2="+solver.problem.q2+"(5min)");
-		}
-		
-		/*for(File instanceFile : instanceFiles){
-			TUPSolver solver = new TUPSolver(9);
-	    	solver.readInstance(instanceFile.getAbsolutePath());
-	    	solver.gamesPerRound = solver.problem.nTeams / 2;
-	    	solver.problem.q1 = solver.problem.nUmpires;
-            solver.problem.q2 = solver.problem.nUmpires / 2;
-	    	solver.solve(900);
-	    	solver.solution();
-	    	solver.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/annealing9/15min/"+extracFile(instanceFile.getName())+"q1="+
-	    			solver.problem.q1+"&q2="+solver.problem.q2+"(15min)");
-		}*/
-		
-		for(File instanceFile : instanceFiles){
-			TUPSolver solver = new TUPSolver(9);
-	    	solver.readInstance(instanceFile.getAbsolutePath());
-	    	solver.gamesPerRound = solver.problem.nTeams / 2;
-	    	solver.problem.q1 = solver.problem.nUmpires;
-            solver.problem.q2 = solver.problem.nUmpires / 2;
-	    	solver.solve(300);
-	    	solver.solution();
-	    	solver.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/annealing9/5min/"+extracFile(instanceFile.getName())+"q1="+
-	    			solver.problem.q1+"&q2="+solver.problem.q2+"(5min)");
-		}
-		
+
+		TUPSolver solver6 = new TUPSolver(7);
+		solver6.readInstance("/Users/Lennart/git/BP-TUP-Thesis/instances/umps30.txt");
+		solver6.gamesPerRound = solver6.problem.nTeams / 2;
+		solver6.problem.q1 = 5;
+		solver6.problem.q2 = 5;
+		solver6.solve(18000);
+		solver6.solution();
+		solver6.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/extra/umps30Annealing="
+				+ solver6.annealingLvl + "&q1=" + solver6.problem.q1 + "&q2=" + solver6.problem.q2);
+
+		TUPSolver solver7 = new TUPSolver(9);
+		solver7.readInstance("/Users/Lennart/git/BP-TUP-Thesis/instances/umps30.txt");
+		solver7.gamesPerRound = solver7.problem.nTeams / 2;
+		solver7.problem.q1 = 5;
+		solver7.problem.q2 = 5;
+		solver7.solve(18000);
+		solver7.solution();
+		solver7.writeSolution("/Users/Lennart/git/BP-TUP-Thesis/outputFiles/extra/umps30Annealing="
+				+ solver7.annealingLvl + "&q1=" + solver7.problem.q1 + "&q2=" + solver7.problem.q2);
+
 	}
-	
-	public static String extracFile(String filename){
-		String exitString = filename.substring(0, filename.length()-4);
+
+	public static String extracFile(String filename) {
+		String exitString = filename.substring(0, filename.length() - 4);
 		return exitString;
 	}
 
